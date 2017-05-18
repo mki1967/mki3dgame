cd81d2ce6c12556356ebba427fe6e9dacbdf86f2
Marcin Kik <mki1967@gmail.com>
2017-05-18 08:34:07 +0200

    reducing messages by ecofreezer

--- ecofreezer.go
+++ ecofreezer.go
@@ -9,8 +9,8 @@ const EcoFreezerDelay = 15 * time.Second // EcoFrezer delay in milliseconds
 
 // EcoFreezer is a goroutine that sets g.PauseRequest if there was no action for a long period.
 func (g *Mki3dGame) EcoFreezer() {
-	fmt.Println("Starting EcoFreezer ...") // tests
-	g.WasAction.Set()                      // set before the first testing
+	// fmt.Println("Starting EcoFreezer ...") // tests
+	g.WasAction.Set() // set before the first testing
 
 	for {
 		time.Sleep(EcoFreezerDelay)
@@ -20,7 +20,7 @@ func (g *Mki3dGame) EcoFreezer() {
 			// fmt.Println("EcoFreezer: Testing: wasAction =", wasAction )
 			if !wasAction {
 				// fmt.Println("EcoFreezer: Setting PauseRequest ...")
-				fmt.Println("EcoFreezer: Setting Pause ...")
+				fmt.Println("AUTO-PAUSE")
 				// g.PauseRequest.Set() // old version
 				g.Paused.Set(true) // new version
 			}
