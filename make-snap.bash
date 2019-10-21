e0850e360f601a8fe9d4113fa10951b82b062198
Marcin Kik <mki1967@gmail.com>
2019-10-21 18:12:07 +0200

    update make-snap.bash -> for making snaps

--- make-snap.bash
+++ make-snap.bash
@@ -19,18 +19,15 @@ echo 'Preparing for AppImage ...'
 mkdir build-snap # do everything in the 'build' directory
 
 
-#  It is assumed that this directory is called 'mki3dgame'
-pushd ../ # Go to the parent directory.
-echo 'rsync-ing ...'
-rsync -av --exclude-from=mki3dgame/rsync-exclude-patterns-snap mki3dgame mki3dgame/build-snap/
-popd    # return to the directory
-
-pushd build-snap/mki3dgame # Go to the build directory.
+pushd build-snap/ # Go to the build directory.
+echo  'copy the relevant files for snap ...'
+cp ../mki3dgame-snap-wrapper.bash .
+rsync -av ../snap .
+rsync -av ../assets .
+cp ../mki3dgame .
 echo 'Building snap ...'
 snapcraft
-mv mki3dgame-snap_*.snap ../
-cd ../
-rm -rf mki3dgame/ # remove the rsync-ed directory
+# mv mki3dgame-snap_*.snap ../
 popd    # return to the directory
 
 echo 'Your snap should be in: ./build-snap/mki3dgame-snap_[...].snap'
